86ae90d117f59db2b1ac80a79476687713ae8354
Harinder Grewal <harri@Harinders-MacBook-Pro.local>
2021-11-16 16:08:17 -0800

    starting

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
-
+import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
 
 export default function SignUpPage(props){
     
     return (
-        <>
-          <h1>Sign up Page!</h1>
-
-          
-        </>
+      <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src="https://i.imgur.com/s4LrnlU.png" /> Sign Up
+        </Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              value={state.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+              value={state.passwordConf}
+              onChange={handleChange}
+              required
+            />
+            <Form.TextArea
+              label="bio"
+              name="bio"
+              placeholder="Tell us more about your dogs..."
+              onChange={handleChange}
+            />
+            <Form.Field>
+              <Form.Input
+                type="file"
+                name="photo"
+                placeholder="upload image"
+                onChange={handleFileInput}
+              />
+            </Form.Field>
+            <Button type="submit" className="btn">
+              Signup
+            </Button>
+          </Segment>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+      </Grid.Column>
+    </Grid>
       );
 }
